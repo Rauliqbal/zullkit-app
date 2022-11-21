459a44754aab571f8eb79e6c28e72d5e64c002c7
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-11-22 06:11:55 +0700

    add component categories and items

---
+++ src/components/Homepage/Categories.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from "vue";
+import CategoriesCard from "../CategoriesCard.vue";
+
+const categories = ref([
+   { title: "Mobile UI Kit", count: 101, image: "categories-1.jpg" },
+   { title: "Fonts", count: 731, image: "categories-2.jpg" },
+   { title: "Icon Set", count: 855, image: "categories-3.jpg" },
+   { title: "Website UI Kit", count: 400, image: "categories-4.jpg" },
+]);
+</script>
+
+<template>
+   <div class="container px-4 mx-auto my-16 md:px-12">
+      <h2 class="mb-4 text-xl font-medium md:mb-0 md:text-lg">Top Categories</h2>
+      <div class="flex flex-wrap -mx-1 lg:-mx-4">
+         <CategoriesCard v-for="category in categories" :title="category.title" :count="category.count" :image="category.image" />
+      </div>
+   </div>
+</template>
--- src/stores/product.js
+++
@@ -1,8 +0,0 @@
-import { defineStore } from "pinia";
-
-export const useProductStore = defineStore({
-   id: "product",
-   state: () => ({
-      products: [],
-   }),
-});
