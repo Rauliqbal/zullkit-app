2999ad75d3389949e90195f2a6ceea28063e4aa8
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-11-21 10:16:54 +0700

    first commit

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from "vue";
+import { useProductStore } from "@/stores/product.js";
+
+const message = ref("this is homepage");
+
+const product = useProductStore();
+</script>
+
+<template>
+   <main>
+      <div class="about">
+         <h1 class="font-bold text-4xl text-green-500">{{ message }}</h1>
+      </div>
+   </main>
+</template>
