8ba73a3f606dc018cd78092cc4be26c8682405ea
Rauliqbal <muhamadrauliqbal.13@gmail.com>
2022-11-22 15:12:14 +0700

    add Pricing page and success view

--- src/components/ItemCard.vue
+++ src/components/ItemCard.vue
@@ -9,15 +9,15 @@ defineProps({
 <template>
    <div class="w-full px-1 my-1 md:w-1/2 lg:my-4 lg:px-4 lg:w-1/3">
       <div class="overflow-hidden border border-gray-200 rounded-xl">
-         <a href="details.html">
+         <router-link to="/product">
             <div class="m-4 overflow-hidden rounded-xl">
                <img :alt="'Thumbnail ' + title" class="block w-full h-auto" :src="'/src/assets/img/' + image" />
             </div>
-         </a>
+         </router-link>
 
          <header class="px-4 mb-4 leading-tight">
             <h1 class="text-lg">
-               <a class="font-semibold text-black no-underline hover:underline" href="#">{{ title }}</a>
+               <router-link to="/product" class="font-semibold text-black no-underline hover:underline">{{ title }}</router-link>
             </h1>
             <span class="block text-sm font-light text-gray-500 no-underline">{{ description }}</span>
          </header>
